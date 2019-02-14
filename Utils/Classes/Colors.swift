24cf8678baa9644924a420204829d4b761ea4a36
Xiangyue Meng <xiangyue.meng@grabtaxi.com>
2019-02-14 17:09:19 +0800

    first

---
+++ Utils/Classes/Colors.swift
@@ -0,0 +1,18 @@
+//
+//  Colors.swift
+//  Utils
+//
+//  Created by Xiangyue Meng on 2019/2/14.
+//  Copyright © 2019 GrabTaxi Pte Ltd. All rights reserved.
+//
+
+import UIKit
+
+public struct Colors {
+  public static var main = UIColor.red
+  public static func printColor() {
+    print("输出主色 \(Date())")
+  }
+}
+
+
